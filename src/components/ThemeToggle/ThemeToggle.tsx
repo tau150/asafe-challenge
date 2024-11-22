e3f2f5e0c6ee22d07590e6aee47f7357b25df4ab
Santiago Nuñez <tau150@hotmail.com>
2024-11-22 20:20:25 +0100

    eslint prettier config

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,21 +1,20 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { Briefcase, Palette } from 'lucide-react'
-import { useTheme } from '@/components/ThemeProvider'
+import React from "react";
+import { Briefcase, Palette } from "lucide-react";
+import { useTheme } from "@/components/ThemeProvider";
 
 export function ThemeToggle() {
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
   return (
-        <button onClick={() => setTheme(theme === 'vibrant' ? "corporate" : 'vibrant')}>
-          {theme === 'vibrant' ? (
-            <Palette className="h-[1.2rem] w-[1.2rem]" />
-          ) : (
-            <Briefcase className="h-[1.2rem] w-[1.2rem]" />
-          )}
-          <span className="sr-only">Toggle theme</span>
-        </button>
-  )
+    <button onClick={() => setTheme(theme === "vibrant" ? "corporate" : "vibrant")}>
+      {theme === "vibrant" ? (
+        <Palette className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Briefcase className="h-[1.2rem] w-[1.2rem]" />
+      )}
+      <span className="sr-only">Toggle theme</span>
+    </button>
+  );
 }
-
